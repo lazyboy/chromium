2c7767473e56160eca965836afc40da24e883dd8
Istiaque Ahmed <lazyboy@chromium.org>
2013-02-05 02:00:54 -0500

    test app

---
+++ tests/chrome-apps/webview_v/main.js
@@ -0,0 +1,18 @@
+var LOG = function(msg) {
+  window.console.log(msg);
+};
+
+window.onload = function() {
+  window.console.log("E: window.onload");
+  var webview = document.querySelector('webview');
+  if (!webview) { LOG('no <webview>'); return; }
+  //webview.src = 'data:text/html,hithere';
+  webview.reload();
+  webview.addEventListener('loadstop', function(e) {
+    LOG('LOADSTOP');
+    LOG('LOADSTOP2');
+  });
+  webview.addEventListener('foobar', function(e) {
+    LOG('::fooevent');
+  });
+};
