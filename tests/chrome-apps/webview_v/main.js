39d5353b8960f39c917cc169a629556d2e5aaba9
Istiaque Ahmed <lazyboy@chromium.org>
2013-02-05 13:54:59 -0500

    call func

--- tests/chrome-apps/webview_v/main.js
+++ tests/chrome-apps/webview_v/main.js
@@ -14,5 +14,9 @@ window.onload = function() {
   });
   webview.addEventListener('foobar', function(e) {
     LOG('::fooevent');
+    LOG('e.a = ' + e.a);
+    e.a();
+    LOG('e.b = ' + e.b);
+    e.b();
   });
 };
