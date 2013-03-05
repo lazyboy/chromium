d6d5ac77f6a1b024652902b2394a0a4be7bd99ff
Istiaque Ahmed <lazyboy@chromium.org>
2013-03-05 18:45:42 -0500

    add fullscreen test app

---
+++ tests/chrome-apps/webview_fullscreen/main.js
@@ -0,0 +1,44 @@
+var $ = function(id) { return document.getElementById(id); };
+var LOG = function(msg) { window.console.log(msg); };
+
+var init = function() {
+  document.onwebkitfullscreenerror = function(e) {
+    LOG('Embedder: onwebkitfullscreenerror: ' + e);
+  };
+  document.onwebkitfullscreenchange = function(e) {
+    LOG('Embedder: onwebkitfullscreenchange: ' + e);
+  };
+};
+
+var goFullscreen = function() {
+  LOG('Embedder go fullscreen');
+  if ((document.fullScreenElement && document.fullScreenElement !== null) ||    // alternative standard method
+      (!document.mozFullScreen && !document.webkitIsFullScreen)) {               // current working methods
+    var elem = document.documentElement;
+    elem = document.getElementById('efullscreen');
+    if (elem.requestFullScreen) {
+      elem.requestFullScreen();
+    } else if (elem.mozRequestFullScreen) {
+      elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullScreen) {
+      elem.webkitRequestFullScreen(
+          Element.ALLOW_KEYBOARD_INPUT);
+    }
+  } else {
+    if (document.cancelFullScreen) {
+      document.cancelFullScreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.webkitCancelFullScreen) {
+      document.webkitCancelFullScreen();
+    }
+  }
+};
+
+window.onload = function() {
+  init();
+  $('e-button').onclick = function(e) {
+    goFullscreen();
+  };
+  LOG('registered click');
+};
