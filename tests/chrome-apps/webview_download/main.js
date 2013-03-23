424b58615cb57af862a7f557fc7fe46dbfd5776f
Istiaque Ahmed <lazyboy@chromium.org>
2013-03-22 22:14:49 -0400

    add permission api on download app

--- tests/chrome-apps/webview_download/main.js
+++ tests/chrome-apps/webview_download/main.js
@@ -1,2 +1,11 @@
 window.onload = function() {
+  document.querySelector('webview').addEventListener('permissionrequest', function(e) {
+    window.console.log('Embedder got permissionrequest');
+    window.console.log('permissionrequest.type: ' + e.type);
+    window.console.log('permissionrequest.requestMethod: '+ e.requestMethod);
+    window.console.log('allowing');
+    e.request.allow();
+    //e.request.deny();
+  });
+  //document.querySelector('webview').src = 'http://jsbin.com/azayiv/1';
 };
