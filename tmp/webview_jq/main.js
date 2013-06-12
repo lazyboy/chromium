5575b27662d5cb98dbd05c9ea4aa8046b768d99f
Istiaque Ahmed <lazyboy@chromium.org>
2013-06-12 16:00:05 -0400

    added document DOMContentLoaded bug

---
+++ tmp/webview_jq/main.js
@@ -0,0 +1,21 @@
+var testD = function() {
+/*
+  console.log('start testD');
+  var webview = document.createElement('webview');
+  webview.setAttribute('id', 'webview');
+  webview.setAttribute('src', 'data:text/html,<body>Guest 1</body>');
+
+  var firstLoad = function() {
+    window.console.log('first load');
+    webview.removeEventListener('loadstop', firstLoad);
+    window.console.log('first load done');
+  };
+  webview.addEventListener('loadstop', firstLoad);
+  document.body.appendChild(webview);
+*/
+};
+
+window.onload = function() {
+  testD();
+};
+
---
+++ tmp/webview_jq/pre.js
@@ -0,0 +1,15 @@
+window.console.log('app: add listener for doc.load');
+window.addEventListener('DOMContentLoaded', function(e) {
+  window.console.log('document.DOMContentLoaded');
+  window.console.log('window.FOO = ' + window.FOO);
+  var w = document.querySelector('webview');
+  window.console.log('webview: ' + w);
+  window.console.log('try to Object.defineProperty');
+  Object.defineProperty(w, 'name', {
+    get: function() { return 'foo-name'; },
+    set: function(value) { window.console.log('ignore setting name: ' + name); },
+    enumerable: true
+  });
+  //document.body.appendChild(w);
+  window.console.log('done try to Object.defineProperty');
+});
