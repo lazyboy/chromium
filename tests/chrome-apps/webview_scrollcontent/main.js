f03311d326bdf56e18287d83a276b91cbfb2a979
Istiaque Ahmed <lazyboy@chromium.org>
2013-06-14 22:53:33 -0400

    update /w object node browser plugin

--- tests/chrome-apps/webview_scrollcontent/main.js
+++ tests/chrome-apps/webview_scrollcontent/main.js
@@ -1,4 +1,5 @@
 var $ = function(id) { return document.getElementById(id); };
+var LOG = function(msg) { window.console.log(msg); };
 
 var makeContent = function() {
   var ret = '';
@@ -10,4 +11,26 @@ window.onload = function() {
   var y = makeContent();
   var ihtml = '<webview src="data:text/html,<body bgcolor=red>Guest' + y + '</body>" style="height: 100px;"></webview>';
   $('webview-container').innerHTML = ihtml;
+
+  // <object>
+  var objectContainer = $('object-container');
+  if (objectContainer) {
+    var z = makeContent();
+    var ihtml2 = '<object src="data:text/html,<body bgcolor=green>Guest ' + z + '</body>"' +
+      ' style="height: 100px;" partition="persist:123" type="application/browser-plugin"></object>';
+    objectContainer.innerHTML = ihtml2;
+  } else {
+    LOG('object-container not found');
+  }
+
+  /*
+  var objectContainer2 = $('object-container2');
+  if (objectContainer2) {
+    var ihtml2 = '<object src="data:text/html,<body bgcolor=green><input type=text ></body>"' +
+      ' style="height: 100px;" partition="persist:123" type="application/browser-plugin"></object>';
+    objectContainer2.innerHTML = ihtml2;
+  } else {
+    LOG('object-container2 not found');
+  }
+  */
 };
