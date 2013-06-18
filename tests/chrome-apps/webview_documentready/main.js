6e73b7fdfa82bd077cf5fc3a094172f79b0ead75
Istiaque Ahmed <lazyboy@chromium.org>
2013-06-18 19:37:14 -0400

    add document DOMContentLoaded sample

---
+++ tests/chrome-apps/webview_documentready/main.js
@@ -0,0 +1,28 @@
+var LOG = function(msg) { window.console.log(msg); };
+
+var doTest = function() {
+  LOG('doTest');
+  var w = document.querySelector('webview');
+  LOG('webview element: ' + w);
+
+  Object.defineProperty(w, 'name', {
+    get: function() { return 'abc'; },
+    set: function(value) {},
+    enumerable: true
+  });
+};
+
+document.addEventListener('DOMContentLoaded', function(e) {
+  LOG('L2. app document.DOMContentLoaded');
+  doTest();
+});
+
+//window.addEventListener('DOMContentLoaded', function(e) {
+//  LOG('app window.DOMContentLoaded');
+//  doTest();
+//});
+
+window.onload = function() {
+  LOG('window.onload');
+};
+
