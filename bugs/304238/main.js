1093d99d62e4bb269bf73668d80425bd93acc12c
Istiaque Ahmed <lazyboy@chromium.org>
2013-10-23 12:42:30 -0400

    add dialog bug repro

---
+++ bugs/304238/main.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+  document.querySelector('webview').addEventListener('dialog', function(e) {
+    window.console.log('DIALOG event');
+    document.getElementById('logs').innerHTML += '<br>Dialog event';
+    e.preventDefault();
+  });
+};
