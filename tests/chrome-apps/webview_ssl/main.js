e8c73d5d3dc8ed8b4b65931c249404f394a04c29
Istiaque Ahmed <lazyboy@chromium.org>
2014-05-01 17:17:06 -0700

    p

---
+++ tests/chrome-apps/webview_ssl/main.js
@@ -0,0 +1,9 @@
+var addWebview = function(src) {
+  var w = document.createElement('webview');
+  w.setAttribute('src', src);
+  document.body.appendChild(w);
+};
+
+window.onload = function() {
+  addWebview('https://www.foobar.com');
+};
