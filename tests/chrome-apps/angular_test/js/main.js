e7dcdaec5f76e719f0775a962bc71476fd1adfa8
Istiaque Ahmed <lazyboy@chromium.org>
2014-12-14 04:10:24 -0500

    add a template

--- tests/chrome-apps/angular_test/js/main.js
+++ tests/chrome-apps/angular_test/js/main.js
@@ -1,19 +1,5 @@
 var cTestApp = angular.module('cTestAppNew', []);
 
-/*
-cTestApp.controller('cTestController', ['$scope', function($scope) {
-  $scope.ready = false;
-  $scope.test = 'bar';
-  var oScope = $scope;
-  window.setTimeout(function() {
-    window.console.log('change stuff');
-    $scope.test = 'new stuff';
-    $scope.ready = true;
-    oScope.ready = true;
-  }.bind(this), 1000);
-}]);
-*/
-
 cTestApp.controller('cTestController', function() {
   this.ready = false;
   this.test = 'bar';
@@ -24,6 +10,19 @@ anotherApp.controller('AnotherController', ['$timeout', function($timeout) {
   this.value = 'Another stuff';
   var self = this;
   $timeout(function() { self.value = 'CHANGED'; }, 3000);
+}]);
+
+function JSONModel(name, title) {
+  this.name = name;
+  this.title = title;
+};
+
+anotherApp.controller('EditTest', ['$scope', function($scope) {
+  var ar = [];
+  ar.push(new JSONModel('John', 'Salut'));
+  ar.push(new JSONModel('Xander', 'Hello'));
+  ar.push(new JSONModel('Tyler', 'Fight'));
+  $scope.data = ar;
 }]);
 
 window.onload = function() {
