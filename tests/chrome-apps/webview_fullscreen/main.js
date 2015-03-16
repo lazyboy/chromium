d14dcf968f5e51bee59c3a8daeb335e0ffd60070
Istiaque Ahmed <lazyboy@chromium.org>
2015-03-16 17:17:29 -0400

    update fullscreen <webview> sample

--- tests/chrome-apps/webview_fullscreen/main.js
+++ tests/chrome-apps/webview_fullscreen/main.js
@@ -8,10 +8,49 @@ var init = function() {
   document.onwebkitfullscreenchange = function(e) {
     LOG('Embedder: onwebkitfullscreenchange: ' + e);
   };
+
+  var w2 = $('w2');
+  if (w2) {
+    w2.addEventListener('consolemessage', function(e) {
+      LOG('G: ' + e.message);
+    });
+    LOG('adding consolemessage handler');
+  }
 };
 
-var goFullscreen = function() {
-  LOG('Embedder go fullscreen');
+var fullscreenImpl = function(elem) {
+  if ((document.fullScreenElement && document.fullScreenElement !== null) ||    // alternative standard method
+      (!document.mozFullScreen && !document.webkitIsFullScreen)) {               // current working methods
+    if (elem.requestFullScreen) {
+      elem.requestFullScreen();
+    } else if (elem.mozRequestFullScreen) {
+      elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullScreen) {
+      elem.webkitRequestFullScreen(
+          Element.ALLOW_KEYBOARD_INPUT);
+    }
+  } else {
+    if (document.cancelFullScreen) {
+      document.cancelFullScreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.webkitCancelFullScreen) {
+      document.webkitCancelFullScreen();
+    }
+  }
+};
+
+var goElementFullscreen = function() {
+  LOG('Embedder element go fullscreen');
+  fullscreenImpl(document.getElementById('efullscreen'));
+};
+
+var goPageFullscreen = function() {
+  LOG('Embedder document go fullscreen');
+  fullscreenImpl(document.documentElement);
+};
+
+/*
   if ((document.fullScreenElement && document.fullScreenElement !== null) ||    // alternative standard method
       (!document.mozFullScreen && !document.webkitIsFullScreen)) {               // current working methods
     var elem = document.documentElement;
@@ -34,6 +73,7 @@ var goFullscreen = function() {
     }
   }
 };
+*/
 
 var exitFullscreen = function() {
   var elem = document.getElementById('efullscreen');
@@ -47,11 +87,40 @@ var exitFullscreen = function() {
 
 window.onload = function() {
   init();
+  /*
+  $('w1').addEventListener('loadstop',
+      function() {
+        LOG('***************************** simple-webview loadstop');
+      });
+  */
   $('e-button').onclick = function(e) {
-    goFullscreen();
+    goElementFullscreen();
   };
   $('e-cancel-button').onclick = function(e) {
     exitFullscreen();
   };
+  $('e-whole-button').onclick = function(e) {
+    goPageFullscreen();
+  };
+
+  var count = 0;
+  $('w2').addEventListener('permissionrequest', function(e) {  // github page.
+    window.console.log('permissionrequest: e.permission: ' + e.permission);
+    if (e.permission !== 'fullscreen') return;
+    window.console.log('w2.permissionrequest.fullscreen');
+    window.console.log('url: ' + e.url);
+    if (count % 2) { window.console.log('allow'); e.request.allow(); }
+    else { window.console.log('deny'); e.request.deny(); }
+    //e.request.allow();
+    ++count;
+  });
+  /*
+  $('w4').addEventListener('permissionrequest', function(e) {  // Youtube.
+    if (e.permission !== 'fullscreen') return;
+    window.console.log('w4.permissionrequest.fullscreen');
+    window.console.log('url: ' + e.url);
+    e.request.allow();
+  });
+  */
   LOG('registered click');
 };
