c4c01a3d23db302b4df5a0198768a173144a2bed
Istiaque Ahmed <lazyboy@chromium.org>
2015-05-10 04:31:43 -0400

    add named window <webview> sample

---
+++ tests/chrome-apps/webview_namedw/main.js
@@ -0,0 +1,22 @@
+var LOG = function(msg) {
+  window.console.log(msg);
+};
+
+var getASrc = function() {
+  return '<body><div id="cross" style="background-color: red; width: 50px; height: 50px;"></body>';
+};
+
+window.onload = function() {
+  var awebview = document.querySelector('#awebview');
+  var bwebview = document.querySelector('#bwebview');
+
+  awebview.addEventListener('newwindow', function(e) {
+    LOG('newwindow event');
+    e.window.attach(bwebview);
+  });
+
+  //awebview.src = getASrc();
+  awebview.src = 'http://jsbin.com/oGUYaxEY/1/';
+  bwebview.src = 'http://jsbin.com/uqePOGa/1/';
+
+};
