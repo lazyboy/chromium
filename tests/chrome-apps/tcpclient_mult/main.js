677118110968b9f215e728192ce463362b1658a8
Istiaque Ahmed <lazyboy@chromium.org>
2016-10-25 19:19:13 -0700

    add multiple sending from tcpclient

---
+++ tests/chrome-apps/tcpclient_mult/main.js
@@ -0,0 +1,35 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.  Use of this
+// source code is governed by a BSD-style license that can be found in the
+// LICENSE file.
+var $ = function(id) { return document.getElementById(id); };
+var LOG = function(msg) { $('logDiv').innerText += msg + '\n'; };
+
+var connect = function() { Client.connect(); };
+var disconnect = function() { Client.disconnect(); };
+var send = function() { Client.send(); };
+var pause = function() { Client.pause(); };
+var unpause = function() { Client.unpause(); };
+var sendInt = null;
+var keepSending = function() {
+  sendInt = window.setInterval(send, 10);
+};
+var clearKeepSending = function() {
+  if (sendInt != null) {
+    window.clearInterval(sendInt);
+  }
+  sendInt = null;
+};
+
+var init = function() {
+  $('connect').onclick = connect;
+  $('disconnect').onclick = disconnect;
+  $('send').onclick = send;
+  $('keepSending').onclick = keepSending;
+  $('clearKeepSending').onclick = clearKeepSending;
+  $('pause').onclick = disconnect;
+  $('unpause').onclick = disconnect;
+};
+
+window.onload = function() {
+  init();
+};
