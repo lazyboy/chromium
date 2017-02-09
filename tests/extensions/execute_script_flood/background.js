e2eae4b8f44aebe4d5059b7a69c9011fca69feb8
Istiaque Ahmed <lazyboy@chromium.org>
2017-02-09 14:10:42 -0800

    push stash

--- tests/extensions/execute_script_flood/background.js
+++ tests/extensions/execute_script_flood/background.js
@@ -5,14 +5,27 @@
 chrome.browserAction.setBadgeText({text: "ON"});
 console.log("Loaded.");
 
+var MAX_ITER = 1000;
+var currentIter = 0;
+var recursiveTry = function() {
+  ++currentIter;
+  if (currentIter > MAX_ITER) return;
+  chrome.tabs.executeScript(undefined, {file: "content.js"}, recursiveTry);
+};
+
 chrome.browserAction.onClicked.addListener(function() {
   console.log('Start');
   // File version
-  for (var i = 0; i < 1000; ++i) {
+  /*
+  for (var i = 0; i < MAX_ITER; ++i) {
     chrome.tabs.executeScript(undefined, {file: "content.js"}, function() {});
   }
+  */
+  // This repros.
+  currentIter = 0;
+  recursiveTry();
   /*
-  for (var i = 0; i < 1000; ++i) {
+  for (var i = 0; i < MAX_ITER; ++i) {
     var str = 'foo ' + i;
     chrome.tabs.executeScript(undefined, {code: "dummy = '" + str + "';" }, function() {});
   }
--- tests/extensions/reload/background.js
+++ tests/extensions/reload/background.js
@@ -2,6 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+console.log('hello');
+chrome.tabs.create('http://www.google.com');
+//chrome.runtime.getURL('foo');
+
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
   // No tabs or host permissions needed!
